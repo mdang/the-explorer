db33871d48f65fe07d4fc4bca7a4b25457be9502
Mike Dang <mike@packstack.co>
2018-02-01 15:19:46 -0600

    Slideshow showing up

--- js/slideshow.js
+++ js/slideshow.js
@@ -12,5 +12,42 @@
     'caption': 'Moonrise'
   }];
 
-  
+  const slideshowContainer = document.getElementsByClassName('slideshow-wrapper')[0];
+
+  let activeSlide = slideshow[0];
+
+  const setActiveSlide = activeSlide => {
+    // Set all slides display none
+
+    //img.style.display = 'block';
+    //document.getElementById('caption').textContent = slide.caption;
+    const slides = slideshowContainer.getElementsByTagName('img');
+    Array.from(slides).forEach(slide => {
+      
+
+      if (slide.getAttribute('src') === activeSlide.image) {
+        slide.style.display = 'block';
+        document.getElementById('caption').textContent = activeSlide.caption;
+      } else {
+        slide.style.display = 'none';
+      }
+    });
+
+
+  };
+
+  slideshow.forEach(slide => {
+    const img = document.createElement("img");
+    img.setAttribute('src', slide.image);
+    img.setAttribute('alt', slide.caption);
+
+    // if (slide.image === activeSlide) {
+    //   img.style.display = 'block';
+    //   document.getElementById('caption').textContent = slide.caption;
+    // }
+
+    slideshowContainer.appendChild(img);
+  });
+
+  setActiveSlide(activeSlide);
 })();
